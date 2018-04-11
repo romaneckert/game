ad642586b4ce39499661d6a8a53a1efdc133b56f
Roman Eckert <mail@romaneckert.de>
2018-04-11 20:29:54 +0200

    add error pug

--- app/middleware/error-handler.js
+++ app/middleware/error-handler.js
@@ -1,11 +1,7 @@
 const logger = require('../service/app-logger');
 
 module.exports = (err, req, res, next) => {
-
     logger.error(err.stack);
-
-    console.log(err);
-
     res.status(500)
-    res.render('error', { error: err })
+    res.render('home/error', { error: err })
 }
